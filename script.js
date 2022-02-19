3a250604a2b41e87cd671f288416201bf79401d9
manishsharma203 <manish1811.sharma@gmail.com>
2022-02-20 01:17:13 +0530

    feat: home page added

---
+++ script.js
@@ -0,0 +1,48 @@
+var currentCarouselNumber = 0;
+const totalCarouselNumber =
+  document.getElementsByClassName("carousel-img").length;
+let carouselImages = document.getElementsByClassName("carousel-img");
+carouselImages[currentCarouselNumber].style.display = "block";
+function pageNext() {
+  if (
+    currentCarouselNumber < totalCarouselNumber - 1 &&
+    currentCarouselNumber >= 0
+  ) {
+    currentCarouselNumber++;
+    for (let i = 0; i < totalCarouselNumber; i++) {
+      if (i === currentCarouselNumber) {
+        carouselImages[i].style.display = "block";
+      } else {
+        carouselImages[i].style.display = "none";
+      }
+    }
+  }
+}
+function pagePrevious() {
+  if (
+    currentCarouselNumber > 0 &&
+    currentCarouselNumber <= totalCarouselNumber - 1
+  ) {
+    currentCarouselNumber--;
+    for (let i = 0; i < totalCarouselNumber; i++) {
+      if (i === currentCarouselNumber) {
+        carouselImages[i].style.display = "block";
+      } else {
+        carouselImages[i].style.display = "none";
+      }
+    }
+  }
+}
+window.onload = function () {
+  setInterval(() => {
+    if (currentCarouselNumber === totalCarouselNumber - 1) {
+      currentCarouselNumber = 0;
+      carouselImages[0].style.display = "block";
+      for (let i = 1; i < totalCarouselNumber; i++) {
+        carouselImages[i].style.display = "none";
+      }
+    } else {
+      pageNext();
+    }
+  }, 2000);
+};
